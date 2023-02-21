f55d1f364dbc7673ed25d54a531da0a7f5fae028
MatshidisoK <asnathke@icloud.com>
2023-02-21 08:31:28 +0200

    javascript added

---
+++ script.js
@@ -0,0 +1,19 @@
+//variables
+
+var myHeader = document.getElementById('my_name');
+var myBio = document.getElementById('my_bio');
+var myAddress =document.getElementById('my_address');
+var address = document.getElementById('address');
+
+
+//bio
+myHeader.innerHTML = myHeader.innerHTML + "Odirile Kekana"
+myBio.innerHTML = myBio.innerHTML + "I fell inlove prommaing simply because I want to be a part of the solution. \n Software development wasn't my initial career path, but I'm glad I discovered it I'm excited to explore a career as a developer and learn more about this line of work from industry professionals.";
+
+//address
+myAddress.innerHTML = myAddress.innerHTML + "Address"
+address.innerHTML = address.innerHTML + "541 Eben Cuyler Drive\nDiepkloof\nSoweto\n1864"; 
+
+//about
+
+ 
